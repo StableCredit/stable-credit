e61dc9659648d7c143146caf8940105eea788b00
bridgerz <bridgerzoske@gmail.com>
2022-10-11 15:35:58 -0700

    Fix task

--- tasks/sendFeeTokens.ts
+++ tasks/sendFeeTokens.ts
@@ -4,13 +4,12 @@ import { send } from "../hardhat.config"
 const fs = require("fs")
 
 import { SEND_FEE_TOKENS } from "./task-names"
-import { MockERC20 } from "../types/MockERC20"
 
 task(SEND_FEE_TOKENS, "Send amount of fee tokens to address")
   .addParam("amount", "Amount of ETH to send to wallet after generating")
   .addParam("address", "Address to fund")
   .setAction(async (taskArgs, { network, ethers }) => {
-    const feeToken = (await ethers.getContract("FeeToken")) as MockERC20
+    const feeToken = await ethers.getContract("FeeToken")
 
     let amount = taskArgs.amount
     let address = taskArgs.address
