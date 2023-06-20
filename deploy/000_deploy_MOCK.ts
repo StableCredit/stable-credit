6cbc2c244a0067cd9bedf1494916c75f463c77db
bridgerz <bridgerzoske@gmail.com>
2023-06-19 18:22:31 -0700

    Fix deployments

--- deploy/000_deploy_MOCK.ts
+++ deploy/000_deploy_MOCK.ts
@@ -246,6 +246,8 @@ const func: DeployFunction = async function (hardhat: HardhatRuntimeEnvironment)
   await (await stableCredit.setCreditPool(creditPoolAddress)).wait()
   // set ambassador
   await (await stableCredit.setAmbassador(ambassadorAddress)).wait()
+  // set riskOracle
+  await (await reservePool.setRiskOracle(riskOracleAddress)).wait()
   // set targetRTD to 20%
   await (await reservePool.setTargetRTD((20e16).toString())).wait()
   // set base fee rate to 5%
